c657986af4bc2dd251bd62f27cefb5e60eb13768
scottsapps <scott@angstreich.net>
2026-03-30 10:22:09 -0400

    Fix missing @Composable import in MainActivity

--- app/src/main/java/com/scottsapps/scotuswatch/MainActivity.kt
+++ app/src/main/java/com/scottsapps/scotuswatch/MainActivity.kt
@@ -27,6 +27,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
